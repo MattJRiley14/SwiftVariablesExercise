4b67704ad142197b1b1a8e8e874e32ae0c10a5e6
Matthew Riley <matthew.riley@barren.kyschool.us>
2023-01-17 09:10:34 -0600

    Initial Commit

---
+++ swiftVariablesExercise.playground/Contents.swift
@@ -0,0 +1,36 @@
+// VARIABLES EXERCISE
+// 1
+print("Matt") // Matt
+
+// 2
+// String, Character, Bool, Int, Float, Double
+
+// 3
+// let
+
+// 4
+var person = "Chris"
+print(person) // Chris
+
+// 5
+person = "Robert"
+print(person) // Robert
+
+// BONUS
+// 1
+var number = 3
+print(number) // 3
+
+// 2
+number = number + 2
+print(number) // 5
+
+// 3
+number += 5
+// number = number + 5 (Same as above)
+print(number) // 10
+
+// 4
+var nums = [1, 8, 9]
+//var nums: Array = [1, 8, 9]
+print(nums) // [1, 8, 9]
